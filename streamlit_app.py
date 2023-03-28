29f7cf28e4a021976ea848e90a306b053bef64ea
Rohitsing45 <129085817+Rohitsing45@users.noreply.github.com>
2023-03-28 19:16:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.header('My parents new healthier dinner')
+streamlit.header('Breakfast Favorites')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
