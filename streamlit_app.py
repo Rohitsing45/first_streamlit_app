1cb80f4ee4dc9bd1a81447962649c567da636b04
Rohitsing45 <129085817+Rohitsing45@users.noreply.github.com>
2023-03-28 19:08:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit as st
-st.title("hello")
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
