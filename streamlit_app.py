222209188babad5de0aa0c29a411b05426ef64a9
Rohitsing45 <129085817+Rohitsing45@users.noreply.github.com>
2023-03-28 19:09:59 +0530

    Update streamlit_app.py
    added new to menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+streamlit.header('My parents new healthier dinner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
